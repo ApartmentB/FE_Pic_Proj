8e73c9f1ad2cb9b5d6108f86db3dbae83a06fdb8
Alex Windle <alexwindle85@gmail.com>
2016-04-07 13:32:28 -0400

    create post // serial form functionality

---
+++ src/js/create_post.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react';
+import Dropzone from 'react-dropzone';
+
+export default class CreatePost extends Component {
+	static propTypes = {
+		onCreate: PropTypes.func.isRequired
+	}
+	
+	constructor(props){
+		super(props);
+		this.state = {
+			preview: "http://www.dreveterinary.com/lib/img/placeholder_small.png"
+		}
+	}
+
+	dropHandler([file]) {
+    this.setState({preview: file.preview});
+    this.file = file;
+  	}
+
+	render() {
+	let { preview } = this.state;
+	let { onCreate } = this.props;
+		return(
+		<div>
+			<h1>Create Post</h1>
+			
+			<SimpleSerialForm onData={ onCreate }>
+				<Dropzone onDrop={ ::this.dropHandler }>
+					<img src={ preview } height="195px" width="195px"/>
+				</Dropzone>
+				<label>
+					Caption:
+					<input type="text"/>
+					<button>Create</button>
+				</label>
+			</SimpleSerialForm>
+			
+			<button>Back</button>
+		</div>
+		)
+	}
+}
