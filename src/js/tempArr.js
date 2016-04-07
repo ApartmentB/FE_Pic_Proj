8c5455bed546f532becc77fa09cd0b6c5717a25c
Ben Lee <leeohsunshine@gmail.com>
2016-04-07 17:30:40 -0400

    Set up temp Data

---
+++ src/js/tempArr.js
@@ -0,0 +1,30 @@
+export default [
+{
+id: 1,
+author: 'aUser',
+status: 'unsolved',
+imgURL: 'http://fillmurray.com/120/120',
+caption: 'Bill',
+},
+{
+id: 2,
+author: 'bUser',
+status: 'unsolved',
+imgURL: 'http://fillmurray.com/420/420',
+caption: 'Will',
+},
+{
+id: 3,
+author: 'cUser',
+status: 'solved',
+imgURL: 'http://fillmurray.com/50/50',
+caption: 'Billy',
+},
+{
+id: 4,
+author: 'dUser',
+status: 'unsolved',
+imgURL: 'http://fillmurray.com/300/300',
+caption: 'NotBill',
+}
+]
